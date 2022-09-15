75ee6e1a05f5750a166a52379ec4c22c6796a31a
Jesús Bocanegra <jesusbodc@gmail.com>
2022-09-15 16:45:34 -0500

    Pruebas del sensor DHT11

--- JFO.ino
+++ JFO.ino
@@ -2,10 +2,23 @@
 #include "SD.h"
 #include "SPI.h"
 #include "Wire.h"
+#include "SensorDHT.h"
+
+#define DHT11TESTPIN 26
+float temperatura, humedad;
+SensorDHT dht(DHT11TESTPIN);
 
 void setup(){
-
+    Serial.begin(115200);
+    dht.empezar();
 }
 void loop(){
-
+    temperatura = dht.leerTemperatura();
+    humedad = dht.leerHumedad();
+    Serial.print("Temperatura: ");
+    Serial.println(temperatura);
+    Serial.print("Humedad: ");
+    Serial.print(humedad);
+    Serial.println(" %");
+    delay(3000);
 }
