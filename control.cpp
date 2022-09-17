8d346009fbfe597b7753b09d4a447c0ff1aad8f8
Jesús Bocanegra <jesusbodc@gmail.com>
2022-09-16 19:17:53 -0500

    Funciones añadidas al controlador

--- control.cpp
+++ control.cpp
@@ -1,11 +1,34 @@
 #include "control.h"
 
-Controlador::Controlador(int aout, int Dat, TwoWire *I2C){
+Controlador::Controlador(int aout, int Dat){
     sensorDeLuz = new SensorDeLuz();
     dht = new SensorDHT(Dat);
-    pinAOut = aout;
+    higrometro = new Higrometro(aout);
 }
 
 float Controlador::medirHumedad(bool forzar){
     return dht->leerHumedad(forzar);
+}
+
+float Controlador::medirTemperatura(bool forzar){
+    return dht->leerTemperatura();
+}
+
+float Controlador::calcularSensacionTermica(){
+    return dht->calcularSensacionTermica();
+}
+
+float Controlador::medirLuz(){
+    return sensorDeLuz->leerNiveldeLuz();
+}
+
+int Controlador::medirHumedadSuelo(){
+    return higrometro->medirHumedad();
+}
+
+bool Controlador::empezar(TwoWire *I2C){
+    sensorDeLuz->empezar(I2C);
+    dht->empezar();
+    higrometro->empezar();
+    return true;
 }
--- control.h
+++ control.h
@@ -1,16 +1,23 @@
 #include "SensorDeLuz.h"
 #include "SensorDHT.h"
+#include "higrometro.h"
 
 class Controlador
 {
 private:
-    // Pin del higrómetro
-    int pinAOut;
+    // Objeto del higrómetro
+    Higrometro* higrometro;
     // Objeto del sensor de luz
     SensorDeLuz* sensorDeLuz;
     // Objeto del sensor de humedad y temperatura del aire
     SensorDHT* dht;
+    // Objeto del I2C
 public:
-    Controlador(int AOut, int Dat, TwoWire *I2C = nullptr);
+    Controlador(int AOut, int Dat);
+    bool empezar(TwoWire *I2C = nullptr);
     float medirHumedad(bool forzar = false);
+    float medirTemperatura(bool forzar = false);
+    float calcularSensacionTermica();
+    float medirLuz();
+    int medirHumedadSuelo();
 };
