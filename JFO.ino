ed44950c8eadc65bcfe2e632396902e9dfc04211
Jesús Bocanegra <jesusbodc@gmail.com>
2022-09-22 00:03:26 -0500

    Añadidas algunas funciones utiles para facilitar la programacion por parte de los estudiantes

--- JFO.ino
+++ JFO.ino
@@ -1,41 +1,80 @@
 #include "control.h"
 
-#define DHT11TESTPIN 26
-#define HIGROMETROTESTPIN 27
-float temperatura, humedad, humedadSuelo, lux;
-Controlador JFO(HIGROMETROTESTPIN,DHT11TESTPIN);
+#define PIN_DHT 26
+#define PIN_HIGROMETRO 27
+
+#pragma region 
+void configurar();
+void mostrarTemperatura();
+void mostrarHumedadDelAire();
+void mostrarLuz();
+void mostrarHumedadDelSuelo();
+void separar();
+void guardarMedidasEnArchivo();
+void esperar(int tiempo = 3000);
+#pragma endregion
+
+Controlador JFO(PIN_HIGROMETRO,PIN_DHT);
 String guardar;
 
 void setup()
 {
-    Serial.begin(115200);
-    Wire.begin();
-    JFO.empezar();
+    configurar();
 }
 void loop()
 {
-    guardar = millis();
-    Serial.println("-----------------------------------------");
-    temperatura = JFO.medirTemperatura();
-    humedad = JFO.medirHumedad();
-    humedadSuelo = JFO.medirHumedadSuelo();
-    lux = JFO.medirLuz();
+    mostrarTemperatura();
+    mostrarHumedadDelAire();
+    mostrarLuz();
+    mostrarHumedadDelSuelo();
+    separar();
+    guardarMedidasEnArchivo();
+    esperar();
+}
+
+void configurar(){
+    Serial.begin(115200);
+    Wire.begin();
+    JFO.empezar();
+    JFO.resetearLog();
+}
+void mostrarTemperatura(){
+    float temperatura = JFO.medirTemperatura();
     Serial.print("Temperatura: ");
-    Serial.println(temperatura);
-    guardar += " " + (String)temperatura;
+    Serial.print(temperatura);
+    Serial.println(F(" °C"));
+}
+void mostrarHumedadDelAire(){
+    float humedadAire = JFO.medirHumedad();
     Serial.print("Humedad del aire: ");
-    Serial.print(humedad);
-    guardar += " " + (String)humedad;
+    Serial.print(humedadAire);
     Serial.println(" %");
+}
+void mostrarLuz(){
+    float luz = JFO.medirLuz();
     Serial.print("Luz: ");
-    Serial.print(lux);
-    guardar += " " + (String)lux;
-    Serial.println(" lx");
+    Serial.print(luz);
+    Serial.println(" lux");
+}
+void mostrarHumedadDelSuelo(){
+    float humedadSuelo = JFO.medirHumedadSuelo();
     Serial.print("Humedad del suelo: ");
     Serial.print(humedadSuelo);
     Serial.println(" %");
-    guardar += " " + (String)humedadSuelo + "\n";
+}
+void separar(){
     Serial.println("-----------------------------------------");
+}
+void guardarMedidasEnArchivo(){
+    medidas datos = JFO.obtenerMedidas();
+    String guardar;
+    guardar = millis();
+    guardar += " " + (String)datos.temperatura;
+    guardar += " " + (String)datos.humedadDelAire;
+    guardar += " " + (String)datos.lux;
+    guardar += " " + (String)datos.humedadDelSuelo + "\n";
     JFO.agregarAlLog(guardar);
-    delay(3000);
+}
+void esperar(int tiempo){
+    delay(tiempo);
 }
