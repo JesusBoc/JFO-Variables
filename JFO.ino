a8934989345dd6d4908b46f75e7018fb924e1e50
Jesús Bocanegra <jesusbodc@gmail.com>
2022-09-21 19:36:12 -0500

    Pruebas del log al archivo

--- JFO.ino
+++ JFO.ino
@@ -4,6 +4,7 @@
 #define HIGROMETROTESTPIN 27
 float temperatura, humedad, humedadSuelo, lux;
 Controlador JFO(HIGROMETROTESTPIN,DHT11TESTPIN);
+String guardar;
 
 void setup()
 {
@@ -16,6 +17,7 @@ void setup()
 }
 void loop()
 {
+    guardar = millis();
     Serial.println("-----------------------------------------");
     temperatura = JFO.medirTemperatura();
     humedad = JFO.medirHumedad();
@@ -23,15 +25,20 @@ void loop()
     lux = JFO.medirLuz();
     Serial.print("Temperatura: ");
     Serial.println(temperatura);
+    guardar += " " + (String)temperatura;
     Serial.print("Humedad del aire: ");
     Serial.print(humedad);
+    guardar += " " + (String)humedad;
     Serial.println(" %");
     Serial.print("Luz: ");
     Serial.print(lux);
+    guardar += " " + (String)lux;
     Serial.println(" lx");
     Serial.print("Humedad del suelo: ");
     Serial.print(humedadSuelo);
     Serial.println(" %");
+    guardar += " " + (String)humedadSuelo + "\n";
     Serial.println("-----------------------------------------");
+    JFO.agregarAlLog(guardar);
     delay(3000);
 }
